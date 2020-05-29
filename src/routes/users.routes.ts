ff7d19424aea8de50ca9032131fe770af81a3a54
Gabriel Elias <gabrieleliascaribe@gmail.com>
2020-05-29 12:46:07 -0300

    1.Completion of the creation of the tmp folder and the upload.ts file. 2.Create AddAvatar in the Migration folder and in Service. 3. Included in Routes and User.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -23,5 +28,23 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    const updateUserAvatarService = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatarService.execute({
+      user_id: request.user.id,
+      avatar_filename: request.file.filename,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  },
+);
 
 export default usersRouter;
--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,14 @@ import routes from './routes';
 
 import './database';
 import 'reflect-metadata';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
-app.use('/files', express.static());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('servidor rodando na porta 3333');
+  // console.log('servidor rodando na porta 3333');
 });
